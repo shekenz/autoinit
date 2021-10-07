045b239389b6fa0081a1457d333d4e05b6d540d2
shekenz <2657910+shekenz@users.noreply.github.com>
2021-10-07 17:42:13 +0200

    Updated webpack.default.js
    Added conditional configs depending on webpack mode.
    Fixed CopyPlugin issue on empty folders.

--- skeletons/webpack.default.js
+++ skeletons/webpack.default.js
@@ -3,40 +3,59 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-	entry : './src/js/index.js',
-	output : {
-		filename : 'bundle.js',
-		path : path.resolve(__dirname, 'dist/js'),
-	},
-	module : {
-		rules : [
-			{
-				test : /\.css$/,
-				use : [
-					MiniCssExtractPlugin.loader,
-					{
-						loader : 'css-loader',
-						options : {
-							importLoaders : 1
-						}
-					},
-					'postcss-loader'
-				]
-			}
+module.exports = (env, argv) => {
+
+	let prodConfigs, devConfigs;
+
+	if(argv.mode === 'development') {
+		// Development specific configs goes here
+		devConfigs = {
+			devtool : 'source-map',
+		}
+	} else if(argv.mode === 'production') {
+		// Production specific configs goes here
+		prodConfigs = {
+
+		}
+	}
+
+	return {
+		...devConfigs,
+		...prodConfigs,
+		entry : './src/js/index.js',
+		output : {
+			filename : 'bundle.js',
+			path : path.resolve(__dirname, 'dist/js'),
+		},
+		module : {
+			rules : [
+				{
+					test : /\.css$/,
+					use : [
+						MiniCssExtractPlugin.loader,
+						{
+							loader : 'css-loader',
+							options : {
+								importLoaders : 1
+							}
+						},
+						'postcss-loader'
+					]
+				}
+			]
+		},
+		plugins: [
+			new CopyPlugin({
+				patterns: [
+					{ from: 'src/index.html', to:'../' },
+					{ from: 'src/img', to:'../img', noErrorOnMissing: true },
+					{ from: 'src/fonts', to:'../fonts', noErrorOnMissing: true },
+				],
+			}),
+			new MiniCssExtractPlugin({
+				filename: "../style/[name].css"
+			}),
+			new CleanWebpackPlugin()
 		]
-	},
-	plugins: [
-		new CopyPlugin({
-			patterns: [
-				{ from: 'src/index.html', to:'../' },
-				{ from: 'src/img', to:'../img' },
-				{ from: 'src/fonts', to:'../fonts' },
-			],
-		}),
-		new MiniCssExtractPlugin({
-			filename: "../style/[name].css"
-		  }),
-		new CleanWebpackPlugin()
-	]
+	};
 }
